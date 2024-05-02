1c06d740b9a26b6404b1c3a36b0ffea7aa20c4ed
Davey Shafik <davey@blackcart.co>
2024-05-02 04:46:54 -0700

    feat: Support fixed currency in MoneyFrom* casters. Extend Major from Minor to avoid duplication.

--- src/Bag/Casts/MoneyFromMinor.php
+++ src/Bag/Casts/MoneyFromMinor.php
@@ -5,27 +5,36 @@ declare(strict_types=1);
 namespace Bag\Casts;
 
 use BackedEnum;
-use Brick\Money\Money;
+use Brick\Money\Exception\UnknownCurrencyException;
+use Brick\Money\Money as BrickMoney;
 use Illuminate\Support\Collection;
 use Override;
+use PrinsFrank\Standards\Currency\CurrencyAlpha3;
 use UnitEnum;
 
 class MoneyFromMinor implements CastsPropertySet, CastsPropertyGet
 {
-    public function __construct(protected string $currencyProperty = 'currency', protected string $locale = 'en_US')
+    public function __construct(protected CurrencyAlpha3|string|null $currency = null, protected ?string $currencyProperty = null, protected string $locale = 'en_US')
     {
     }
 
     #[Override]
     public function set(string $propertyType, string $propertyName, Collection $properties): mixed
     {
-        $amount = $properties->first($propertyName);
+        $amount = $properties->get($propertyName);
 
-        if ($amount instanceof Money) {
+        if ($amount instanceof BrickMoney) {
             return $amount;
         }
 
-        $currency = $properties->get($this->currencyProperty);
+        $currency = $this->currency;
+        if ($this->currencyProperty !== null) {
+            $currency = $properties->get($this->currencyProperty, $this->currency);
+        }
+
+        if ($currency === null) {
+            throw new UnknownCurrencyException('No currency found');
+        }
 
         if ($currency instanceof BackedEnum) {
             $currency = $currency->value;
@@ -35,14 +44,20 @@ class MoneyFromMinor implements CastsPropertySet, CastsPropertyGet
             $currency = $currency->name;
         }
 
-        return Money::ofMinor($amount, $currency);
+        return $this->makeMoney($amount, $currency);
     }
 
     #[Override]
     public function get(string $propertyName, Collection $properties): mixed
     {
-        /** @var Money $money */
+        /** @var BrickMoney $money */
         $money = $properties->get($propertyName);
+
         return $money->formatTo($this->locale);
+    }
+
+    protected function makeMoney(mixed $amount, int|string|CurrencyAlpha3|null $currency): BrickMoney
+    {
+        return BrickMoney::ofMinor($amount, $currency);
     }
 }

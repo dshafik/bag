2fb7c83f781f237188bd6c601f28fefa65c90e37
Davey Shafik <davey@blackcart.co>
2024-05-02 04:56:20 -0700

    fix: Cast variadic parameter values correctly on input

--- src/Bag/Concerns/WithCasts.php
+++ src/Bag/Concerns/WithCasts.php
@@ -4,17 +4,23 @@ declare(strict_types=1);
 
 namespace Bag\Concerns;
 
+use Bag\Collection;
 use Bag\Property\Value;
+use Bag\Property\ValueCollection;
+use Illuminate\Support\Collection as LaravelCollection;
 
 trait WithCasts
 {
-    protected static function castValues(mixed $values, mixed $aliases, mixed $properties, mixed $extraProperties): mixed
+    protected static function castValues(Collection $values, LaravelCollection $aliases, ValueCollection $properties, LaravelCollection $extraProperties): mixed
     {
         $values = $values->map(function ($value, $inputKey) use ($values, $aliases, $properties, $extraProperties) {
             $key = $aliases['input'][$inputKey] ?? $inputKey;
 
             if (isset($extraProperties[$key])) {
-                return $value;
+                /** @var Value $last */
+                $last = $properties->last();
+
+                return ($last->inputCast)('extra', collect(['extra' => $value]));
             }
 
             /** @var Value $property */
@@ -22,6 +28,7 @@ trait WithCasts
 
             return ($property->inputCast)($key, $values);
         });
+
         return $values;
     }
 }

5811a0049c6e4e8d20618398f3a1086d6cda6420
Davey Shafik <davey@blackcart.co>
2024-05-02 04:48:00 -0700

    refactor: Remove Variadic attribute, use parameter definition instead.

--- src/Bag/Concerns/WithVariadics.php
+++ src/Bag/Concerns/WithVariadics.php
@@ -4,22 +4,12 @@ declare(strict_types=1);
 
 namespace Bag\Concerns;
 
-use Bag\Attributes\Variadic;
 use Bag\Property\ValueCollection;
-use ReflectionClass;
 
 trait WithVariadics
 {
-    protected static function variadic(ReflectionClass $class, ValueCollection $properties): array
+    protected static function isVariadic(ValueCollection $properties): bool
     {
-        $variadics = $class->getAttributes(Variadic::class);
-        /** @var Variadic $variadic */
-        $variadic = isset($variadics[0]) ? $variadics[0]->newInstance() : null;
-
-        $isVariadic = false;
-        if ($variadic !== null || $properties->last()->variadic) {
-            $isVariadic = true;
-        }
-        return array($variadic, $isVariadic);
+        return $properties->last()->variadic;
     }
 }

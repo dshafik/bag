f26bb748f2f9953dc42fa6b1a638a48c4baa00c4
Davey Shafik <davey@php.net>
2025-01-23 23:29:37 -0800

    fix: ensure nested bags are cast correctly with Eloquent
    fixes #70

--- tests/Fixtures/Models/CastedModelLegacy.php
+++ tests/Fixtures/Models/CastedModelLegacy.php
@@ -10,11 +10,13 @@ use Illuminate\Database\Eloquent\Model;
 use Tests\Fixtures\Collections\BagWithCollectionCollection;
 use Tests\Fixtures\Values\BagWithCollection;
 use Tests\Fixtures\Values\HiddenParametersBag;
+use Tests\Fixtures\Values\OptionalPropertiesBag;
 use Tests\Fixtures\Values\TestBag;
 
 /**
  * @property int $id
  * @property TestBag $bag
+ * @property OptionalPropertiesBag $optionals_bag
  * @property HiddenParametersBag $hidden_bag
  * @property Collection $collection
  * @property BagWithCollectionCollection $custom_collection
@@ -23,10 +25,11 @@ class CastedModelLegacy extends Model
 {
     protected $table = 'testing';
 
-    protected $fillable = ['bag', 'hidden_bag', 'collection', 'custom_collection'];
+    protected $fillable = ['bag', 'optionals_bag', 'hidden_bag', 'collection', 'custom_collection'];
 
     protected $casts = [
         'bag' => TestBag::class,
+        'optionals_bag' => OptionalPropertiesBag::class,
         'hidden_bag' => HiddenParametersBag::class,
         'collection' => AsBagCollection::class . ':' . TestBag::class,
         'custom_collection' => AsBagCollection::class . ':' . BagWithCollection::class,
